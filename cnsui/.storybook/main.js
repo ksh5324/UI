acb1225b3ec79aa50e2cf9db5ce7448cd547fd30
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-08-08 15:43:45 +0900

    test

--- cnsui/.storybook/main.js
+++ cnsui/.storybook/main.js
@@ -1,5 +1,12 @@
 module.exports = {
   stories: ["../src/components/**/*.stories.js"],
+  refs: {
+    "design-system": {
+      title: "My design system",
+      //👇 The url provided by Chromatic when it was deployed
+      url: "https://www.chromatic.com/apps?accountId=62efeca7d7a33abd903ac012",
+    },
+  },
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",

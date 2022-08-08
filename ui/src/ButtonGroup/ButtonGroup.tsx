935412a280c84a1f82b843480c6468925c172926
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-08-08 23:40:50 +0900

    buttonGroup

--- ui/src/ButtonGroup/ButtonGroup.tsx
+++ ui/src/ButtonGroup/ButtonGroup.tsx
@@ -7,9 +7,56 @@ export type ButtonGroupProps = {
   /** 버튼을 우측에 보여줍니다. */
   rightAlign?: boolean;
   /** 버튼과 버튼사이의 간격을 설정합니다. */
-  gap: number | string;
+  gap: string | number;
   /** 버튼 그룹에서 보여줄 버튼들 */
   children: React.ReactNode;
   /* 스타일 커스터마이징 하고싶을 때 사용 */
   className?: string;
 };
+
+/**
+ * 여러개의 `Button` 컴포넌트를 보여주고 싶거나, 버튼을 우측에 정렬하고 싶을 땐 `ButtonGroup` 컴포넌트를 사용하세요.
+ */
+const ButtonGroup = ({
+  direction,
+  rightAlign,
+  children,
+  gap,
+  className,
+}: ButtonGroupProps) => {
+  return (
+    <ButtonGroupStyle defaultValue={gap} slot={direction}>
+      <div
+        className={`${className} ${
+          direction === "row" ? "marginLeft" : "marginTop"
+        } ${rightAlign ? "rightAlignStyle" : ""}`}
+      >
+        {children}
+      </div>
+    </ButtonGroupStyle>
+  );
+};
+
+const ButtonGroupStyle = styled.div`
+  div {
+    display: flex;
+    flex-direction: ${(prop) => prop.slot};
+  }
+  .marginLeft div + div {
+    margin-left: ${(prop) => prop.defaultValue};
+  }
+  .marginTop div + div {
+    margin-top: ${(prop) => prop.defaultValue};
+  }
+
+  .rightAlignStyle {
+    justify-content: flex-end;
+  }
+`;
+
+ButtonGroup.defaultProps = {
+  direction: "row",
+  gap: "0.5rem",
+};
+
+export default ButtonGroup;
